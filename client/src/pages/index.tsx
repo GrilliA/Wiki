8511fb782baaff41a55224d73357ecf29c1feefa
Oriabure trust <trust.oriabure@ascom.com>
2025-11-02 13:03:36 +0100

    remove some stuff

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,24 @@
+import GemmaList from "@/components/List/List";
+import GemmaListItem from "@/components/List/ListItem";
 import { PageTemplate } from "@/components/PageTemplate/PageTemplate";
+import { Title } from "@mantine/core";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return <PageTemplate> text</PageTemplate>;
+  const { push } = useRouter();
+
+  return (
+    <PageTemplate>
+      <Title>Our Links</Title>
+      <GemmaList isHoverable>
+        <GemmaListItem
+          handleClick={() => {
+            push("/auth/signup");
+          }}
+        >
+          Sign up
+        </GemmaListItem>
+      </GemmaList>
+    </PageTemplate>
+  );
 }
