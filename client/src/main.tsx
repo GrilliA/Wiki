90cb2c1d2f6dcd7e7b736d38e0b9f3a1c988b3bb
oriabure trust <trust4u@hotmail.it>
2025-11-14 17:56:42 +0100

    add redirects file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,21 +1,4 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
-import WikiProvider from "./WikiProvider.tsx";
-import { store } from "./state/store.ts";
-import { Provider } from "react-redux";
-import { WikiLoader } from "./components/Loader/Loader.tsx";
+import { Root } from "./Root.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <WikiProvider>
-          <App />
-          <WikiLoader />
-        </WikiProvider>
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<Root />);
