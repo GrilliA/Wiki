eb2901e80da80aaaa37beef3d3d32df3e9b2452a
Oriabure trust <trust.oriabure@ascom.com>
2025-11-15 17:20:23 +0100

    test

--- client/src/components/HeroSection/HeroSection.tsx
+++ client/src/components/HeroSection/HeroSection.tsx
@@ -11,8 +11,6 @@ export const HeroSection = () => {
   };
   return (
     <div className={classes.wrapper}>
-      <Overlay color="#000" opacity={0.65} zIndex={1} />
-
       <div className={classes.inner}>
         <Title className={classes.title}>
           Welcome to{" "}
